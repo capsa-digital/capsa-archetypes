52a5ad081a3e1466f7a981b862a8a7c17822b60f
michaellif <michael.lifschitz@gmail.com>
2020-08-08 22:55:21 -0400

    cleanup

--- capsa-infra-it/src/main/kotlin/digital/capsa/it/aggregate/Account.kt
+++ capsa-infra-it/src/main/kotlin/digital/capsa/it/aggregate/Account.kt
@@ -1,20 +1,10 @@
 package digital.capsa.it.aggregate
 
-import org.apache.commons.lang3.RandomStringUtils
-import java.util.Locale
-
-class Account(
-        var domain: String? = null,
-        var password: String? = null,
-        var lang: Locale? = null
-) : AbstractAggregate("Account") {
+class Account : AbstractAggregate("Account") {
 
     fun library(init: Library.() -> Unit) = initAggregate(Library(), init)
 
-    override fun construct() {
-        lang = lang ?: Locale.ENGLISH
-        domain = domain ?: RandomStringUtils.randomNumeric(10)
-    }
+    override fun construct() {}
 
     override fun onCreate(context: AggregateBuilderContext) {}
 }
