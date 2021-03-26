6d428936cf43f4a210a857d3b83d5370d637e5f3
michaellif <michael.lifschitz@gmail.com>
2021-03-26 08:52:14 -0400

    Actuator test is working

--- capsa-archetypes-app/build.gradle.kts
+++ capsa-archetypes-app/build.gradle.kts
@@ -8,8 +8,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation(project(":capsa-archetypes-command"))
     implementation(project(":capsa-archetypes-query"))
-    testImplementation("org.springframework.boot:spring-boot-starter-aop")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.named<BootJar>("bootJar") {
